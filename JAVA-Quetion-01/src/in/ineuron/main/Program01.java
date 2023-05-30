98e16cf6b1722405e851e7cfcf48e1ef2cad4d50
devavratwadekar <devavrat.wadekar@gmail.com>
2023-05-30 15:28:39 +0530

    First Assignment

---
+++ JAVA-Quetion-01/src/in/ineuron/main/Program01.java
@@ -0,0 +1,56 @@
+package in.ineuron.main;
+
+interface Shape {
+    double calculateArea();
+    double calculatePerimeter();
+}
+
+class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+class Triangle implements Shape {
+    private double side1;
+    private double side2;
+    private double side3;
+
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public double calculateArea() {
+        // Using Heron's formula to calculate the area of a triangle
+        double s = (side1 + side2 + side3) / 2;
+        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
+
+    public double calculatePerimeter() {
+        return side1 + side2 + side3;
+    }
+}
+
+public class Program01 {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5.0);
+        System.out.println("Circle Area: " + circle.calculateArea());
+        System.out.println("Circle Perimeter: " + circle.calculatePerimeter());
+
+        Shape triangle = new Triangle(3.0, 4.0, 5.0);
+        System.out.println("Triangle Area: " + triangle.calculateArea());
+        System.out.println("Triangle Perimeter: " + triangle.calculatePerimeter());
+    }
+}
