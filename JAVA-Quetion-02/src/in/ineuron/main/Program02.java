90ee1ed85c78e4638ab8042beb7ba449499829f9
devavratwadekar <devavrat.wadekar@gmail.com>
2023-05-30 15:30:55 +0530

    Second Assignment

---
+++ JAVA-Quetion-02/src/in/ineuron/main/Program02.java
@@ -0,0 +1,20 @@
+package in.ineuron.main;
+
+class Parent {
+    public Parent() {
+        System.out.println("Parent class constructor");
+    }
+}
+
+class Child extends Parent {
+    public Child() {
+        super(); // Invokes the parent class constructor
+        System.out.println("Child class constructor");
+    }
+}
+
+public class Program02 {
+    public static void main(String[] args) {
+        Child child = new Child();
+    }
+}
